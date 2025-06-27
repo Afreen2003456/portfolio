a9d9e0a5e26c6e1230f111544c45f5fdde239acf
haaris519 <hs898@nau.edu>
2025-06-27 01:48:58 -0700

    added git actions

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   // App Router is enabled by default in Next.js 13+
   output: 'export',
   trailingSlash: true,
+  basePath: '/haaris-portfolio',
+  assetPrefix: '/haaris-portfolio',
   images: {
     unoptimized: true
   }
